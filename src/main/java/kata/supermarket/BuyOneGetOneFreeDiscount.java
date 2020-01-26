6e3393e8c284924aade9e0feec96c01ec88ff9d2
Ali <60323377+aozmen121@users.noreply.github.com>
2020-01-26 21:24:36 +0000

    Compile and implement the minimum to pass the null item test

---
+++ src/main/java/kata/supermarket/BuyOneGetOneFreeDiscount.java
@@ -0,0 +1,20 @@
+package kata.supermarket;
+
+import java.math.BigDecimal;
+
+/**
+ * Buy one get one free promotion handler class
+ */
+public class BuyOneGetOneFreeDiscount {
+
+    /**
+     * Applys the prmotion based on the item
+     *
+     * @param item Item data
+     *
+     * @return amount of promotion applied
+     */
+    public BigDecimal applyDiscount(Item item) {
+        return BigDecimal.ZERO;
+    }
+}
